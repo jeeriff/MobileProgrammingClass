397eec92074fceb1e101eb74c23a275aaeb94d6c
jeeriff <thegreatwhiteboo@Yahoo.com>
2016-05-24 14:07:31 -0400

    Initial Commit

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by Matthew Harrison on 5/24/16.
+//  Copyright © 2016 Matthew Harrison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calculator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by Matthew Harrison on 5/24/16.
+//  Copyright © 2016 Matthew Harrison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
